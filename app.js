cefa89e0cf945d63dcbd87f1928222907a752ba7
Bill Boughton <boughton.william@gmail.com>
2017-03-27 17:04:47 -0600

    post still isnt working

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8080
 const cars = require('./routes/main.js')
 
 app.use('/cars', cars)
 
-app.use(bodyParser())
-// app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 
 app.listen(PORT, function() {
--- routes/main.js
+++ routes/main.js
@@ -8,14 +8,23 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/', (req, res) => {
-  // var test1 = req.body.manuf
-  var test2 = req.body.model
+router.post('/new', (req, res) => {
+  console.log(req.body);
+  var test1 = req.body.manuf1
+  var test2 = req.body.model1
   var test3 = req.body.eng_hp
   console.log(test1, test2, test3);
-  knex('typeOfCar').insert({eng_hp:test3,  model:test2}).returning(['id', 'eng_hp', 'manuf', 'model'])
+  knex('typeOfCar').insert({eng_hp:test3,  manuf:test1, model:test2})
     .then(result => {
-      res.send(result)
+      res.send(200)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  var id = req.params.id
+  knex('typeOfCar').where('id', id).del()
+    .then(result => {
+      res.send(200)
     })
 })
 
